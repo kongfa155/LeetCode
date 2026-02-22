4abdcdface1e6626f3f337f0ac6bb8e48d1cb5c2
Dao Cong Pha <mongpha90@gmail.com>
2026-02-22 09:09:54 +0700

    optimal solution for TwoSum

--- Python/TwoSum.py
+++ Python/TwoSum.py
@@ -4,9 +4,28 @@
 
 # You can return the answer in any order.
 
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 if(nums[i] + nums[j] == target):
+#                     return [i,j]
+
+
+
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         new_list = [] 
+#         for i in range(len(nums)):
+#             new_list.append(nums[i])
+#             j = target - nums[i]
+#             if j in new_list and new_list.index(j) != i:
+#                 return [i, new_list.index(j)]
 class Solution(object):
     def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if(nums[i] + nums[j] == target):
-                    return [i,j]
+        new_dict = {}
+        for i in range(0,len(nums)):
+            j = target - nums[i]
+            if j in new_dict:
+                return [i, new_dict[j]]
+            new_dict[nums[i]] = i
